5a7cca1b55a4e6c2bd0698e03b0233e17d0f6300
DanielDerefaka <bryancagedx@gmail.com>
2024-08-04 18:59:35 +0100

    done

--- components/shared/CreateUserForm.tsx
+++ components/shared/CreateUserForm.tsx
@@ -158,7 +158,7 @@ const router = useRouter()
           <Label htmlFor="profileImg">Please Connect Your Wallet</Label> <br />
           
         <button className="bg-black-2 text-white mt-2 rounded-sm p-2 px-2 py-4">
-        <ConnectWallett/>
+        {/* <ConnectWallett/> */}
         </button>
         </div>
 
