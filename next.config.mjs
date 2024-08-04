ee852156ca514d17ef825f53fdebe3507a5e9901
DanielDerefaka <bryancagedx@gmail.com>
2024-08-04 17:50:46 +0100

    add

--- next.config.mjs
+++ next.config.mjs
@@ -24,6 +24,10 @@ const nextConfig = {
       // Ignoring TypeScript errors during build
       ignoreBuildErrors: true,
     },
+
+    eslint:{
+      ignoreDuringBuilds: true
+  }
 };
 
 export default nextConfig;
